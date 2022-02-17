9784cdaebe3dce135c0acce205bf0dae4a4e0f33
yangmengwei925 <58463843+yangmengwei925@users.noreply.github.com>
2022-02-17 14:13:09 +0800

    修改

--- src/views/dashboard/admin/components/PieChart.vue
+++ src/views/dashboard/admin/components/PieChart.vue
@@ -3,8 +3,8 @@
 </template>
 
 <script>
-import echarts from 'echarts'
-require('echarts/theme/macarons') // echarts theme
+// import echarts from 'echarts'
+// require('echarts/theme/macarons') // echarts theme
 import resize from './mixins/resize'
 
 export default {
@@ -42,37 +42,37 @@ export default {
   },
   methods: {
     initChart() {
-      this.chart = echarts.init(this.$el, 'macarons')
+    //   this.chart = echarts.init(this.$el, 'macarons')
 
-      this.chart.setOption({
-        tooltip: {
-          trigger: 'item',
-          formatter: '{a} <br/>{b} : {c} ({d}%)'
-        },
-        legend: {
-          left: 'center',
-          bottom: '10',
-          data: ['Industries', 'Technology', 'Forex', 'Gold', 'Forecasts']
-        },
-        series: [
-          {
-            name: 'WEEKLY WRITE ARTICLES',
-            type: 'pie',
-            roseType: 'radius',
-            radius: [15, 95],
-            center: ['50%', '38%'],
-            data: [
-              { value: 320, name: 'Industries' },
-              { value: 240, name: 'Technology' },
-              { value: 149, name: 'Forex' },
-              { value: 100, name: 'Gold' },
-              { value: 59, name: 'Forecasts' }
-            ],
-            animationEasing: 'cubicInOut',
-            animationDuration: 2600
-          }
-        ]
-      })
+    //   this.chart.setOption({
+    //     tooltip: {
+    //       trigger: 'item',
+    //       formatter: '{a} <br/>{b} : {c} ({d}%)'
+    //     },
+    //     legend: {
+    //       left: 'center',
+    //       bottom: '10',
+    //       data: ['Industries', 'Technology', 'Forex', 'Gold', 'Forecasts']
+    //     },
+    //     series: [
+    //       {
+    //         name: 'WEEKLY WRITE ARTICLES',
+    //         type: 'pie',
+    //         roseType: 'radius',
+    //         radius: [15, 95],
+    //         center: ['50%', '38%'],
+    //         data: [
+    //           { value: 320, name: 'Industries' },
+    //           { value: 240, name: 'Technology' },
+    //           { value: 149, name: 'Forex' },
+    //           { value: 100, name: 'Gold' },
+    //           { value: 59, name: 'Forecasts' }
+    //         ],
+    //         animationEasing: 'cubicInOut',
+    //         animationDuration: 2600
+    //       }
+    //     ]
+    //   })
     }
   }
 }
